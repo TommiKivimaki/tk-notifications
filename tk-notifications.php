1ce10160d665e2b277b77b5c559a82a149ac7e2a
Tommi Kivimäki <tommi.kivimaki@caravanstudio.fi>
2018-02-26 17:54:29 +0200

    Version numbering

--- tk-notifications.php
+++ tk-notifications.php
@@ -4,24 +4,12 @@ Plugin Name:  TK Notifications
 Description:  Sends notifications to subscribed users when a new post is published.
 Plugin URI:
 Author:       Tommi Kivimäki
-Version:      0.1
+Version:      0.1.0
 Text Domain:  tk-notifications
 Domain Path:  /languages
 License:      
 License URI:  
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version
-2 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-with this program. If not, visit: https://www.gnu.org/licenses/
 */
 
 
